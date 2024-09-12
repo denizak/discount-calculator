c3599dd822316d775fb90aab9169880b61eabbb3
deni zakya <denizakya@gmail.com>
2024-09-12 11:27:57 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'discount-calculator',
+  webDir: 'www'
+};
+
+export default config;
